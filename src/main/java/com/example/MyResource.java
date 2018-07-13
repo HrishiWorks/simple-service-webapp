f80cb83dd4b0ac7207a7a2b102b6f89967003bd5
GS-0754 <GS-0754@GS-2157.GSLAB.COM>
2018-07-13 14:05:37 +0530

    Initial Commit

---
+++ src/main/java/com/example/MyResource.java
@@ -0,0 +1,35 @@
+package com.example;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Root resource (exposed at "myresource" path)
+ */
+@Path("myresource")
+public class MyResource {
+
+    /**
+     * Method handling HTTP GET requests. The returned object will be sent
+     * to the client as "text/plain" media type.
+     *
+     * @return String that will be returned as a text/plain response.
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getIt() {
+    	JsonObject myObject = Json.createObjectBuilder()
+    	        .add("name", "Agamemnon")
+    	        .add("age", 32)
+    	        .build();
+
+    	
+    	return myObject.toString();
+    }
+    
+    
+}
